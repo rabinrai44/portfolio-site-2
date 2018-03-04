6deb725035a95ce36705a87a12e1a9c42c0b6f96
rabinrai44 <rabinrai44@gmail.com>
2018-03-04 00:31:57 -0500

    Added hero and setting up global css with override bootstrap essential vars

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('serve', ['sass'], () => {
         server: './src'
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/**/*.scss'], ['sass']);
     gulp.watch('src/*.html').on('change', browswerSync.reload);
 });
 
