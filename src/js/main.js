6291f3bf6267b0df9f62f0121d8a407869a5043d
rabinrai44 <rabinrai44@gmail.com>
2018-03-05 16:43:09 -0500

    Added smooth scroll effect

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,23 @@
 //         $('nav').removeClass('nav-sticky');
 //     }
 // })
+
+// Smooth Scrolling
+
+$(document).ready(function(){ 
+    $('nav li a').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+         && location.hostname == this.hostname) { 
+             var $target = $(this.hash); 
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+              if ($target.length) { 
+                  var targetOffset = $target.offset().top; 
+                  $('html,body') .animate({
+                      scrollTop: targetOffset
+                        }, 1000);
+                    return false; 
+                        } 
+                    } 
+                }); 
+        });
+// reference: https://www.learningjquery.com/2007/09/animated-scrolling-with-jquery-12
