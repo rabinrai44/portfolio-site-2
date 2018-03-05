e6155b3a6566fb36ad93c0024facb7baa8031800
rabinrai44 <rabinrai44@gmail.com>
2018-03-05 15:44:14 -0500

    Updated navigation

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Sticky Navigation for future wish
+// $(window).on('scroll', function() {
+//     if ($(window).scrollTop()) {
+//         $('nav').addClass('nav-sticky');
+//     }
+//     else {
+//         $('nav').removeClass('nav-sticky');
+//     }
+// })
