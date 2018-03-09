05318a2b89aabde95fde6fb82b6ab0d495bfd186
rabinrai44 <rabinrai44@gmail.com>
2018-03-09 01:11:35 -0500

    Added build function for production ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const   gulp            = require('gulp'),
         browswerSync    = require('browser-sync').create(),
-        sass            = require('gulp-sass');
+        sass            = require('gulp-sass'),
+        imagemin        = require('gulp-imagemin'),
+        del             = require('del'),
+        usemin          = require('gulp-usemin'),
+        rev             = require('gulp-rev'),
+        cssnano         = require('gulp-cssnano'),
+        uglify          = require('gulp-uglify');
 
 // Compile sass into CSS & Auto-inject into browsers 
 gulp.task('sass', () => {
@@ -8,9 +14,9 @@ gulp.task('sass', () => {
             'node_modules/bootstrap/scss/bootstrap.scss', 
             'src/scss/*.scss'
             ])
-            .pipe(sass({outputStyle: 'compressed'})
+            .pipe(sass()
             .on('error', sass.logError))
-            .pipe(gulp.dest('src/css'))
+            .pipe(gulp.dest('src/temp/css'))
             .pipe(browswerSync.stream());
 });
 
@@ -21,8 +27,49 @@ gulp.task('js', () => {
         'node_modules/jquery/dist/jquery.min.js', 
         'node_modules/popper.js/dist/umd/popper.min.js'
     ])
-    .pipe(gulp.dest('src/js'))
+    .pipe(gulp.dest('src/temp/js'))
     .pipe(browswerSync.stream());
+});
+
+// Delete Dist Folder Task (deploying to the github-pages it need to have 'docs' folder intead of dist)
+gulp.task('deleteDistFolder', () => {
+    return del('./docs');
+});
+
+// Image Optimizes Task 
+gulp.task('optimizeImages', ['deleteDistFolder'], () => {
+    return gulp.src(['./src/assets/images/**/*'])
+    .pipe(imagemin({
+        progressive: true,
+        interlaced: true,
+        multipass: true
+    }))
+        .pipe(gulp.dest('./docs/assets/images'));
+});
+
+// Usemin Task
+gulp.task('usemin', ['sass', 'js'], () => {
+    return gulp.src('./src/index.html')
+        .pipe(usemin({
+            css: [() =>{return rev()}, () => {return cssnano()}],
+            js: [() => {return rev()}, () => {return uglify()}]
+        }))
+        .pipe(gulp.dest('./docs'));
+});
+
+// Usemin Trigger 
+gulp.task('useminTrigger', ['deleteDistFolder'], () => {
+    gulp.start('usemin');
+});
+
+// Previewing Dist Task
+gulp.task('previewDist', () => {
+    browswerSync.init({
+        notify: false,
+        server: {
+            baseDir: 'docs'
+        }
+    });
 });
 
 // Static server + watching scss/html files 
@@ -38,3 +85,6 @@ gulp.task('serve', ['sass'], () => {
 
 // Run gulp task
 gulp.task('default', ['js', 'serve']);
+
+// Build Task for Production Ready 
+gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'useminTrigger']);
